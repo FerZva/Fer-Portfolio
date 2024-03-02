67b9b731e53cf87345b78cc1c9321c86b24d93b2
FerZva <fernandzva2019@outlook.es>
2024-03-02 09:45:14 -0600

    change description on layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Fernando | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  description: "Fernando is a Frontend developer with 4 years of experience.",
 };
 
 export default function RootLayout({
