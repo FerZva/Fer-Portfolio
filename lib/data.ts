02586d02bb5e3a35f2ff4d1c852467a7a7b351d6
FerZva <79551517+FerZva@users.noreply.github.com>
2024-11-12 16:07:45 +0000

    URUZ AI work example updated

--- lib/data.ts
+++ lib/data.ts
@@ -2,8 +2,8 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
-import corpcommentImg from "@/public/EasyBank.jpg";
-import rmtdevImg from "@/public/Countries-API.jpg";
+import uruzai from "@/public/EasyBank.jpg";
+import rmtdevImg from "@/public/URUZAI.png";
 import worldbushidoImg from "@/public/worldbushidofederation.png";
 
 export const links = [
@@ -66,14 +66,14 @@ export const projectsData = [
     description:
       "Landing page for a bank that challenge me throghout the developmenent process",
     tags: ["React", "Typescript", "SASS", "Vite"],
-    imageUrl: corpcommentImg,
+    imageUrl: uruzai,
     liveView: "https://clinquant-lamington-8be0c2.netlify.app",
   },
   {
-    title: "REST Countries API with color theme switcher",
+    title: "URUZ AI",
     description:
-      "This is one of my favourites proyect becuase the challenge to display data from an API was very interesting",
-    tags: ["React", "TypeScript", "Next.js", "SASS", "Styled Components"],
+      "This is one of my favorite projects because the challenge of integrating artificial intelligence was amazing, and the results are unbelievable.",
+    tags: ["NextJS", "TypeScript", "Tailwind", "NodeJS", "Prisma", "Open AI", "Stripe", "Clerk"],
     imageUrl: rmtdevImg,
     liveView: "https://amazing-moxie-8d5504.netlify.app/",
   },
