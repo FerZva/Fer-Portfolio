8a3ddba7648ee0e65dbab36c978728e18df2118c
FerZva <fernandzva2024@gmail.com>
2025-08-03 16:59:30 -0600

    Update email address for actions and contact component

--- components/contact.tsx
+++ components/contact.tsx
@@ -33,8 +33,8 @@ export default function Contact() {
 
       <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
-        <a className="underline" href="mailto:fernandzva2019@outlook.es">
-          fernandzva2019@outlook.es
+        <a className="underline" href="mailto:fernandzva2019@gmail.com">
+          fernandzva2019@gmail.com
         </a>{" "}
         or through this form.
       </p>
